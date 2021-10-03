7df2d0553302f81079e6dfbd0d8b3929e82d9e1e
Izze <56184561+i-cortez@users.noreply.github.com>
2021-10-03 16:46:19 -0700

    Implemented Dijkstra's Forward Search

--- p1/src/nm_pathfinder.py
+++ p1/src/nm_pathfinder.py
@@ -1,5 +1,6 @@
+from heapq import heappop, heappush
+from math import sqrt
 def find_path (source_point, destination_point, mesh):
-
     """
     Searches for a path from source_point to destination_point through the mesh
 
@@ -14,113 +15,100 @@ def find_path (source_point, destination_point, mesh):
         A list of boxes explored by the algorithm
     """
 
+    # List of points from source to destination is returned by the function
     path = []
-    boxes = {}
-    endpoint_boxes = []
-    box_path = []
+    # A list of boxes explored by the algorithm is returned by the function
+    boxes = []
+    # Box the source point is found in
+    source_box = None
+    # Box the destination point is found in
+    destination_box = None
+    # Keeps track of the precise (x, y) position within a box
     detail_points = {}
 
+    # Iterate through all boxes in the image checking for source and destination points
+    for box in mesh["boxes"]:
+        # Check if the source point is within bounds of the current box
+        if box[0] <= source_point[0] <= box[1] and box[2] <= source_point[1] <= box[3]:
+            source_box = box
+            boxes.insert(0, source_box)
+            print("source point: ", source_point)
+            print("source box: ", source_box)
+        # Check if the destinatin point is within bounds of the current box
+        if source_point != destination_point and box[0] <= destination_point[0] <= box[1] and box[2] <= destination_point[1] <= box[3]:
+            destination_box = box
+            print("destination point: ", destination_point)
+            print("destination box: ", destination_box)
     
-    print("source point", source_point)
-    print("destination point", destination_point)
-    #print(mesh['adj'])
-    for box in mesh['boxes']:
-        if source_point[0] <= box[1] and source_point[0] >= box[0] and source_point[1] >= box[2] and \
-           source_point[1] <= box[3]:
-                endpoint_boxes.insert(0,box)
-        if destination_point[0] <= box[1] and destination_point[0] >= box[0] and destination_point[1] >= box[2] and \
-             destination_point[1] <= box[3]:
-                endpoint_boxes.insert(1,box)
-
+    # Verify that the boxes are valid
+    if source_box == None or destination_box == None:
+        print("invalid box coordinates: no path found")
+        return None, None
     
-    # Start breadth first search
-    queue = None
-    parent = None
-    
+    # Verify that the source and destination points are not in the same box
+    if source_box == destination_box:
+        path.append(source_point)
+        path.append(destination_point)
+        boxes.append(destination_box)
+        return path, boxes
 
-    if len(endpoint_boxes) < 2:
-        print("No path!")
+    # Begin Dijkstra's Forward Search
+    #
+    # Maps boxes and the distance to the source box
+    dist = {source_point: 0}
+    # Maps boxes and previous-hop boxes
+    prev = {source_box: None}
+    # Queue maintains a list of pairs (box, cost to previous)
+    queue = [(source_box, 0)]
+    detail_points.update({source_box: source_point})
+    heappush(queue, (source_box, 0))
 
-    else:
-        detail_points[endpoint_boxes[0]] = [source_point[1], source_point[0]]
-        detail_points[endpoint_boxes[1]] = [destination_point[1], destination_point[0]]
-        queue = [endpoint_boxes[0]] 
-        parent = {endpoint_boxes[0]: None}
-        while queue:
-            current_box = queue.pop(0)
-            if current_box == endpoint_boxes[1]:
-                while current_box is not None:  #Finds path by going backwards
-                    box_path.insert(0, current_box)
-                    current_box = parent[current_box]  
-            else:
-                for adj in mesh['adj'][current_box]:
-                    if adj not in parent:
-                        parent[adj] = current_box
-                        queue.append(adj)        
-        if len(box_path) < 1 or box_path[-1] is not endpoint_boxes[1]:
-            print("No path!")
-
-    print(box_path)
-    print(endpoint_boxes[0])
-    
-    index = 0
-
-    while index < len(box_path) - 1:
-        x_left = max(box_path[index][2], box_path[index + 1][2])
-        x_right = min(box_path[index][3], box_path[index + 1][3])
-        y_down = max(box_path[index][0], box_path[index + 1][0])
-        y_up = min(box_path[index][1], box_path[index + 1][1])
-        px = detail_points[box_path[index]][0]
-        py = detail_points[box_path[index]][1]
-
-        detail_points[box_path[index + 1]] = [None, None]
-        
-
-        if px < x_left:  #if src x < b2x1
-            if py >= y_down and py <= y_up:  # if src y in between b2y1 and b2y2
-                detail_points[box_path[index + 1]][0] = x_left
-                detail_points[box_path[index + 1]][1] = py
-            elif py < y_down:
-                detail_points[box_path[index + 1]][0] = x_left
-                detail_points[box_path[index + 1]][1] = y_down
-            else:
-                detail_points[box_path[index + 1]][0] = x_left
-                detail_points[box_path[index + 1]][1] = y_up
-
-        elif px >= x_left and px <= x_right:    #if b2x1 <= px <= b2x2
-            detail_points[box_path[index + 1]][0] = px
-            detail_points[box_path[index + 1]][1] = y_down
-        
+    while queue:
+        current = heappop(queue)
+        # Check if the current box is the destination box
+        if current[0] == destination_box:
+            boxes.append(current[0])
+            path.append(destination_point)
+            # Construct the path by iterating through all previous boxes
+            while current[0]:
+                print("current: ", current[0])
+                print("previous: ", prev[current[0]])
+                if prev[current[0]]:
+                    print("if: ", current[0])
+                    path.append(detail_points[current[0]])
+                current = (prev[current[0]], None)
+            path.append(source_point)
+            path.reverse()
+            print("path: ", path)
+            print("boxes: ", boxes)
+            return path, boxes
         else:
-            if py >= y_down and py <= y_up:  # if src y in between b2y1 and b2y2
-                detail_points[box_path[index + 1]][0] = x_right
-                detail_points[box_path[index + 1]][1] = py
-            elif py < y_down:
-                detail_points[box_path[index + 1]][0] = x_right
-                detail_points[box_path[index + 1]][1] = y_down
-            else:
-                detail_points[box_path[index + 1]][0] = x_right
-                detail_points[box_path[index + 1]][1] = y_up
-       # print(box_path[index],  "completed")
-        index += 1
+            for next_box in mesh["adj"][current[0]]:
+                if next_box not in prev:
+                    prev.update({next_box: current[0]})
+                    # Calculate the next point to move to
+                    next_point = get_dest_point(next_box, current[0])
+                    current_point = detail_points[current[0]]
+                    distance = dist[current_point] + get_distance(current_point, next_point)
+                    detail_points.update({next_box: next_point})
+                    dist.update({next_point: distance})
+                    boxes.append(current[0])
+                    heappush(queue, (next_box, distance))
+    return None, None
 
-    #print("DETAIL POINTS:", detail_points)
-    
-    
-    detail_points[endpoint_boxes[1]] = [destination_point[1],destination_point[0]]
-    if len(detail_points) == 1:
-        print([source_point[0],source_point[1]])
-        print([destination_point[0],destination_point[1]])
-    index = 0
-    for box in box_path:
-        print(detail_points[box])
-        path.append(detail_points[box][::-1]) 
-        index += 1
+def get_distance(a, b):
+    return sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
-   # path.append([source_point[0], source_point[1]])
-   # path.append([destination_point[0], destination_point[1]])
-    print("path", path)
-    print("endpoint_boxes", endpoint_boxes)
-    #print(path, endpoint_boxes)
-    return path, endpoint_boxes
+def get_dest_point(a, b):
+    x1 = max(a[0], b[0])
+    x2 = min(a[1], b[1])
+    y1 = max(a[2], b[2])
+    y2 = min(a[3], b[3])
+
+    if x1 < x2: x = x1
+    else: x = x2
+    if y1 < y2: y = y1
+    else: y = y2
+
+    return (x,y)
 
