2d03167bf5e2cd173c90171a6120397efc9bfc3b
Izze <56184561+i-cortez@users.noreply.github.com>
2021-10-08 22:40:23 -0700

    Updated mcts_vanilla

--- p2-mcts/src/mcts_vanilla.py
+++ p2-mcts/src/mcts_vanilla.py
@@ -8,30 +8,52 @@ explore_faction = 2.
 
 def traverse_nodes(node, board, state, identity):
     """ Traverses the tree until the end criterion are met.
-
     Args:
         node:       A tree node from which the search is traversing.
         board:      The game setup.
         state:      The state of the game.
         identity:   The bot's identity, either 'red' or 'blue'.
-
     Returns:        A node from which the next stage of the search can proceed.
-
     """
-    pass
+
+    max_node = node
+    #identity
+    while max_node.untried_actions == []:
+        max = 0
+        my_kids = list(node.child_nodes.keys())
+        for kid in my_kids:
+            cur_node =  node.child_nodes.get(kid)
+            if cur_node.visits == 0:
+                max_node = cur_node
+                break
+            else:
+                V = cur_node.wins/cur_node.visits
+                rest = math.sqrt(math.log(node.visits)/cur_node.visits)
+                UCT = V + (explore_faction*rest)
+                if UCT > max:
+                    max = UCT
+                    max_node = cur_node
+
+    if max_node.visits == 0:
+        return max_node
+
+    else:
+        while max_node.untried_actions != []:
+            new_node = expand_leaf(max_node, board, state)
+            max_node.untreied_actions.pop(0)
+        return new_node
+
+
     # Hint: return leaf_node
 
 
 def expand_leaf(node, board, state):
     """ Adds a new leaf to the tree by creating a new child node for the given node.
-
     Args:
         node:   The node for which a child will be added.
         board:  The game setup.
         state:  The state of the game.
-
     Returns:    The added child node.
-
     """
 
     new_state = board.next_state(state, node.untried_actions[0])
@@ -42,35 +64,52 @@ def expand_leaf(node, board, state):
 
 def rollout(board, state):
     """ Given the state of the game, the rollout plays out the remainder randomly.
-
     Args:
         board:  The game setup.
         state:  The state of the game.
-
     """
-    pass
+    for move in moves:
+        total_score = 0.0
+
+        # Sample a set number of games where the target move is immediately applied.
+        for r in range(ROLLOUTS):
+            rollout_state = board.next_state(state, move)
+
+            # Only play to the specified depth.
+            for i in range(MAX_DEPTH):
+                if board.is_ended(rollout_state):
+                    break
+                rollout_move = random.choice(board.legal_actions(rollout_state))
+                rollout_state = board.next_state(rollout_state, rollout_move)
+
+            total_score += outcome(board.owned_boxes(rollout_state),
+                                   board.points_values(rollout_state))
+
+        expectation = float(total_score) / ROLLOUTS
+
+        # If the current move has a better average score, replace best_move and best_expectation
+        if expectation > best_expectation:
+            best_expectation = expectation
+            best_move = move
 
 
 def backpropagate(node, won):
     """ Navigates the tree from a leaf node to the root, updating the win and visit count of each node along the path.
-
     Args:
         node:   A leaf node.
         won:    An indicator of whether the bot won or lost the game.
-
     """
-    pass
+    while node.parent != None:
+        node.wins += won
+        node.visits += 1
 
 
 def think(board, state):
     """ Performs MCTS by sampling games and calling the appropriate functions to construct the game tree.
-
     Args:
         board:  The game setup.
         state:  The state of the game.
-
     Returns:    The action to be taken.
-
     """
     identity_of_bot = board.current_player(state)
     root_node = MCTSNode(parent=None, parent_action=None, action_list=board.legal_actions(state))
